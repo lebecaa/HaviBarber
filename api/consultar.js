471dc29341895b58f5197600b64dc232d729f0c6
L3BECA <rebecaslv350@gmail.com>
2024-09-17 16:06:50 -0300

    Correção do Projeto

--- api/consultar.js
+++ api/consultar.js
@@ -1,17 +1,18 @@
-import { MongoClient } from 'mongodb';
+import { connectToDB } from '../lib/db';
 
-const uri = process.env.MONGO_URI;
-let client;
-let clientPromise;
-
-if (!client) {
-  client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  clientPromise = client.connect();
-}
-
-export async function connectToDB() {
-  if (!client.isConnected()) {
-    await clientPromise;
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    try {
+      const db = await connectToDB();
+      const collection = db.collection('consultas');
+      
+      const consultas = await collection.find({}).toArray();
+      
+      return res.status(200).json(consultas);
+    } catch (error) {
+      return res.status(500).json({ error: 'Erro ao conectar ao banco de dados' });
+    }
+  } else {
+    res.status(405).json({ error: 'Método não permitido' });
   }
-  return client.db();
 }
